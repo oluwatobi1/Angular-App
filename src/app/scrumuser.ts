a38cf3ea721fb0328587ac3295be2f1e5aef7199
tobi_akin <akintunlesetobi@gmail.com>
2020-10-08 19:27:24 +0000

    stach:

---
+++ src/app/scrumuser.ts
@@ -0,0 +1,11 @@
+export class Scrumuser {
+
+constructor(
+public email: string,
+public fullname: string,
+public password: string,
+public type: string,
+        public projectName:string
+){}
+
+}
