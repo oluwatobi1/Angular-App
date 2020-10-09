7e5d75f3ab09adb3514a196044a9114d8e8228cc
oluwatobi1 <akintunlesetobi@gmail.com>
2020-10-09 16:32:07 +0100

    Routing Login and signup components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { ScrumboardComponent } from './scrumboard/scrumboard.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomepageComponent},
+  {path: 'home', component:HomepageComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'signup', component:SignupComponent},
+  {path: 'scrumboard', component:ScrumboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
