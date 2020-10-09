04206b6e3bf11a6c7d2822c727c77125db842e97
oluwatobi1 <akintunlesetobi@gmail.com>
2020-10-09 15:17:39 +0100

    Debug1: re-positioned routing file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-root',
@@ -8,5 +6,5 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'akintunlesetobichatscrum';
+  title = 'projectAngular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+
+
 import { AppRoutingModule } from './app-routing.module';
-
-
-
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
@@ -17,8 +16,8 @@ import { FormsModule } from '@angular/forms';
     LoginComponent,
   ],
   imports: [
-    AppRoutingModule,
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
     FormsModule
   ],
