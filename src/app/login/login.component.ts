2bd5c5d95ee4baa137ce115b4f3acf2c50012d47
oluwatobi1 <akintunlesetobi@gmail.com>
2020-10-09 11:38:32 +0100

    Updated login component(changed document codes formatting from vim oneline to proper format

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,1 +1,22 @@
-import { Component, OnInit } from '@angular/core'; import { Scrumuser } from '../scrumuser';   @Component({   selector: 'app-login',   templateUrl: './login.component.html',   styleUrls: ['./login.component.css'] }) export class LoginComponent implements OnInit {    scrumUserModel = new Scrumuser('','','','', '');    constructor() { }    ngOnInit(): void {  }  }
+import { Component, OnInit } from '@angular/core';
+import { Scrumuser } from '../scrumuser';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  scrumUserModel = new Scrumuser('','','','', '');
+
+  constructor() { }
+
+  ngOnInit(): void { 
+  
+  }
+
+    
+  }
+
