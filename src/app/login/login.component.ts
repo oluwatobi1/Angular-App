5558e215f4dc3c22bb93d566c1f32e153fe27082
oluwatobi1 <akintunlesetobi@gmail.com>
2020-10-13 16:02:06 +0100

    Lab 8: Backend api

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScrumdataService } from '../scrumdata.service';
 import { Scrumuser } from '../scrumuser';
 
 
@@ -9,12 +10,26 @@ import { Scrumuser } from '../scrumuser';
 })
 export class LoginComponent implements OnInit {
 
-  scrumFormModel = new Scrumuser('','','','', '');
+  scrumFormData = new Scrumuser('','','','', '');
+  public data: any;
+  public error: any;
 
-  constructor() { }
 
-  ngOnInit(): void { 
-  
+  constructor(private _scrumdataservice: ScrumdataService) { }
+
+  ngOnInit(): void {   
+  }
+  onLoginSubmit(){
+    this._scrumdataservice.login(this.scrumFormData).subscribe(
+      data=>{
+        console.log("Success: ", data);
+        this.data = data},
+      error=>{
+        console.log("Error! ", error);
+        this.error = error        
+      }
+        
+       )
   }
 
     
--- src/app/scrumdata.service.ts
+++ src/app/scrumdata.service.ts
@@ -12,6 +12,7 @@ export class ScrumdataService {
   constructor(private _http: HttpClient) {}
 
   _url= "https://liveapi.chatscrum.com/scrum/api/scrumusers/"
+  _loginUrl="https://liveapi.chatscrum.com/scrum/api-token-auth/"
 
   public httpOptions = {
     headers: new HttpHeaders({'Content-Type':'application/json'})
@@ -22,4 +23,10 @@ export class ScrumdataService {
     return this._http.post<any>(this._url, {'email': user['email'], 'password': user['password'],
      'full_name': user['fullname'], 'usertype': user['type'], 'projname': user['projectName']}, this.httpOptions);
       }
+
+
+  login(user){
+    return this._http.post<any>(this._loginUrl, {'email':user['email'], 'password':user['password'],
+                                                  'projname':user['projectName']})
+  }
 }
