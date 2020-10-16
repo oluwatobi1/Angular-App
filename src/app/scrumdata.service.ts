299ba7a132d2ed07fb6d31382a0e16afa9d4c427
oluwatobi1 <akintunlesetobi@gmail.com>
2020-10-16 14:12:45 +0100

    bal:solved(temp)1

--- src/app/scrumdata.service.ts
+++ src/app/scrumdata.service.ts
@@ -3,30 +3,26 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Scrumuser } from './scrumuser';
 
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class ScrumdataService {
 
-  constructor(private _http: HttpClient) {}
+  constructor(private _http : HttpClient) {}
 
-  _url= "https://liveapi.chatscrum.com/scrum/api/scrumusers/"
-  _loginUrl="https://liveapi.chatscrum.com/scrum/api-token-auth/"
+  _url = 'https://liveapi.chatscrum.com/scrum/api/scrumusers/'
+  _loginUrl = 'https://liveapi.chatscrum.com/scrum/api-token-auth/';
 
   public httpOptions = {
-    headers: new HttpHeaders({'Content-Type':'application/json'})
+    headers : new HttpHeaders({'Content-Type' : 'application/json'})
   }
 
+  signup(user : Scrumuser){
+    return this._http.post<any>(this._url, {'email' : user['email'], 'password' : user['password'], 
+    'full_name' : user['fullname'], 'usertype' : user['usertype']}, this.httpOptions);
+  }
 
-  signup(user: Scrumuser){
-    return this._http.post<any>(this._url, {'email': user['email'], 'password': user['password'],
-     'full_name': user['fullname'], 'usertype': user['type'], 'projname': user['projectName']}, this.httpOptions);
-      }
-
-
-  login(user){
-    return this._http.post<any>(this._loginUrl, {'email':user['email'], 'password':user['password'],
-                                                  'projname':user['projectName']})
+  login(user) {
+    return this._http.post<any>(this._loginUrl, {'username': user['email'], 'password': user['password'], 'project': user['projname']}, this.httpOptions)
   }
 }
